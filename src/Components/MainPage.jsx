7064835824f0e79c2832fd77eaa6bfaa395dbd0d
Xuan Ng <118996751+yeexng@users.noreply.github.com>
2023-06-12 13:40:04 +0800

    Updated css effect

--- src/Components/MainPage.jsx
+++ src/Components/MainPage.jsx
@@ -50,34 +50,42 @@ const MainPage = () => {
           <TransitionGroup component={null}>
             <CSSTransition classNames="fade" timeout={500}>
               <div className="my-intro">
-                <p className="about-me">
-                  I'm Sam YeeXuan Ng, <br />a Malaysian who pursued my education{" "}
+                <p className="">
+                  <div className="about-me">
+                    I'm Sam YeeXuan Ng, <br />a Malaysian who pursued my
+                    education <br />
+                    in Ireland, graduating with an LLB degree.
+                  </div>
                   <br />
-                  in Ireland, graduating with an LLB degree.
+                  <div className="about-me">
+                    While studying law, I discovered my passion for coding and
+                    decided to dive into the tech industry.
+                    <br /> I completed a rigorous coding bootcamp and emerged as
+                    a full-stack developer.
+                  </div>
                   <br />
+                  <div className="about-me">
+                    Currently, I work as a Community Operations Analyst at
+                    Covalen, a tech company. However, my ultimate goal is to
+                    transition into a full-time role in the tech industry, where
+                    I can combine my legal background and coding skills to
+                    create innovative solutions.
+                  </div>
                   <br />
-                  While studying law, I discovered my passion for coding and
-                  decided to dive into the tech industry.
-                  <br /> I completed a rigorous coding bootcamp and emerged as a
-                  full-stack developer.
+                  <div className="about-me">
+                    I'm driven by a desire to make a positive impact through
+                    technology and continuously expand my knowledge in the
+                    field. In my free time, I enjoy exploring new technologies,
+                    attending tech meetups, and engaging in coding challenges.{" "}
+                    <br />
+                    I'm excited about the future and the endless possibilities
+                    in the tech industry.{" "}
+                  </div>
                   <br />
-                  <br />
-                  Currently, I work as a Community Operations Analyst at
-                  Covalen, a tech company. However, my ultimate goal is to
-                  transition into a full-time role in the tech industry, where I
-                  can combine my legal background and coding skills to create
-                  innovative solutions.
-                  <br />
-                  <br />
-                  I'm driven by a desire to make a positive impact through
-                  technology and continuously expand my knowledge in the field.
-                  In my free time, I enjoy exploring new technologies, attending
-                  tech meetups, and engaging in coding challenges. <br />
-                  I'm excited about the future and the endless possibilities in
-                  the tech industry. <br />
-                  <br />
-                  Let's connect and explore the exciting world of technology
-                  together!
+                  <div className="about-me">
+                    Let's connect and explore the exciting world of technology
+                    together!
+                  </div>
                 </p>
               </div>
             </CSSTransition>
